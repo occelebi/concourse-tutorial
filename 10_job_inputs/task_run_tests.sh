6bac0905777297601767a35780b73e87883db2c5
Cem Celebi <cem@smallpdf.com>
2017-03-07 13:09:50 +0100

    Update

--- 10_job_inputs/task_run_tests.sh
+++ 10_job_inputs/task_run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-set -e
+set -e -x
 
 # task script is in resource-tutorial/10_job_inputs/ folder
 # application input is in gopath/src/github.com/cloudfoundry-community/simple-go-web-app folder
 # $GOPATH is gopath/ folder
-export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/cloudfoundry-community/simple-go-web-app/Godeps/_workspace
-cd gopath/src/github.com/cloudfoundry-community/simple-go-web-app/
+export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/occelebi/deneme/Godeps/_workspace
+cd gopath/src/github.com/occelebi/deneme/
 
-go test ./...
+go test -v ./...
